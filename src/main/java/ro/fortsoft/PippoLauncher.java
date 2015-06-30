8869aa1ed704c5f5406407c4720db100206dac51
danjee <daniel.jipa@fortsoft.ro>
2015-06-30 15:47:16 +0300

    initial commit

---
+++ src/main/java/ro/fortsoft/PippoLauncher.java
@@ -0,0 +1,15 @@
+package ro.fortsoft;
+
+import ro.pippo.core.Pippo;
+
+/**
+ * Run application from here.
+ */
+public class PippoLauncher {
+
+    public static void main(String[] args) {
+        Pippo pippo = new Pippo(new PippoApplication());
+        pippo.start();
+    }
+
+}
